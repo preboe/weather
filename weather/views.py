58243221a1c2a0497edd0a938cc48ee3c9080fd3
arkadie <preboe25@gmail.com>
2020-09-30 21:21:39 +0700

    add in weather

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+    appid = '8561ee4246a907b4d7596a1a456c1095'
+    url = 'api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+
+
+    city = 'Tomsk'
+
+
     return render(request, 'weather/index.html')
